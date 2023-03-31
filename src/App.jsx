96b600270cd10892ba37f5ff3edade84f0544302
sabrinagrifante <sabrinagrifante@gmail.com>
2023-03-31 20:12:34 -0300

    resolução exercício 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Header />
       <main className="container mt-4">
-        {/* algo precisa ser modificado aqui... */}
+        <Outlet />
       </main>
     </div>
   );
